ebebe1985346a64f4504174b2b0c555eb7bb1da0
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-12-18 01:12:03 +1300

    Shellcheck command (#46)
    * command: fix bug, fix shellcheck issues
    
    * test(molecule): install recommended pkgs in verify.yml
    
    * remove one lingering line for command module
    
    * fix placement of variable

--- plugins/modules/command.sh
+++ plugins/modules/command.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021 Markus Weippert
 # GNU General Public License v3.0 (see https://www.gnu.org/licenses/gpl-3.0.txt)
 
-PARAMS="
+export PARAMS="
     cmd=raw_params=_raw_params/str/r
     uses_shell=_uses_shell/bool//false
     chdir/str
@@ -11,19 +11,19 @@ PARAMS="
     creates/str
     removes/str
 "
-RESPONSE_VARS="
+export RESPONSE_VARS="
     start end delta cmd
     stdout/str/a stderr/str/a rc/int/a
 "
-SUPPORTS_CHECK_MODE=""
+cmd=
 
 init() {
-    stdout=""
-    stderr=""
-    start=""
-    end=""
-    delta=""
-    rc="0"
+    export stdout=""
+    export stderr=""
+    export start=""
+    export end=""
+    export delta=""
+    export rc="0"
     [ -n "$executable" ] || executable="/bin/sh"
     out="$(mktemp)" && err="$(mktemp)"
 }
@@ -32,26 +32,27 @@ main() {
     local ts_start ts_end s_delta
     [ -z "$chdir" ] || try cd "$chdir"
 
-    [ -z "$creates" ] || ! ls -d -- $creates >/dev/null 2>/dev/null || {
+    [ -z "$creates" ] || ! ls -d -- "$creates" >/dev/null 2>/dev/null || {
         stdout="skipped, since $creates exists"; exit 0
     }
-    [ -z "$removes" ] || ls -d -- $removes >/dev/null 2>/dev/null || {
+    [ -z "$removes" ] || ls -d -- "$removes" >/dev/null 2>/dev/null || {
         stdout="skipped, since $removes does not exist"; exit 0
     }
 
     ts_start="$(date +%s)"
-    [ -z "$uses_shell" ] && {
-        echo "$cmd" | xargs sh -c 'exec "$@"' -- >"$out" 2>"$err"; rc=$?; :
-    } || {
-        "$executable" -c "$cmd" >"$out" 2>"$err"; rc=$?; :
-    }
+    if [ -z "$uses_shell" ]; then
+        echo "$cmd" | xargs sh -c 'exec "$@"' -- >"$out" 2>"$err"
+        rc=$?
+    else
+        "$executable" -c "$cmd" >"$out" 2>"$err"
+        rc=$?
+    fi
     ts_end="$(date +%s)"
     s_delta=$((ts_end - ts_start))
 
     start="$(date -d "@$ts_start" "+%Y-%m-%d %H:%M:%S").000000"
     end="$(date -d "@$ts_end" "+%Y-%m-%d %H:%M:%S").000000"
-    delta="$(printf "%d:%.2d:%.2d.000000" \
-        $((delta / 3600)) $((delta % 3600 / 60)) $((delta % 60)))"
+    delta="$(printf "%d:%02d:%02d.000000" $((s_delta / 3600)) $((s_delta % 3600 / 60)) $((s_delta % 60)))"
     stdout="$(cat "$out")"
     stderr="$(cat "$err")"
     changed
